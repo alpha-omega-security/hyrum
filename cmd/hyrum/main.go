491c4f2c7e4a30fe282a5fd53ad3f078cb6b57e0
Andrew Nesbitt <andrewnez@gmail.com>
2026-08-20 15:18:42 +0100

    Index configured dependency activations

--- cmd/hyrum/main.go
+++ cmd/hyrum/main.go
@@ -66,7 +66,7 @@ func printUsage() {
 const usageText = `hyrum: generate and run Hyrum's tests
 
 Usage:
-  hyrum surface [--dep name] [--scope scope] [--include path] [--exclude path] [--json] [path]
+  hyrum surface [--config file] [--dep name] [--scope scope] [--include path] [--exclude path] [--json] [path]
   hyrum gen     [--config file] [--dep name] [--scope scope] [--include path] [--exclude path] [--out dir] [--work dir] [--backend name] [--run] [path]
   hyrum check   --dep name[@version] [--tests dir] [path]
   hyrum corpus  --upstream purl --out dir [--dependent URL[@ref]] [--discover N]
